8ec638047f4bb74d46d192edf1fb6c9514c3ef53
Juan Diego Rodríguez Guarín <minionpaul85@gmail.com>
2023-10-08 01:13:53 -0500

    Visualización departamentos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Home } from './Home';
+import { Department } from './Department';
+import { Employee } from './Employee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h3 className="justify-center-content m-3">React JS FrontEnd</h3>
+
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item- m-1">
+              <Link className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item- m-1">
+              <Link className="btn btn-light btn-outline-primary" to="/department">
+                Department
+              </Link>
+            </li>
+            <li className="nav-item- m-1">
+              <Link className="btn btn-light btn-outline-primary" to="/employee">
+                Employee
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/department" element={<Department />} />
+          <Route path="/employee" element={<Employee />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
